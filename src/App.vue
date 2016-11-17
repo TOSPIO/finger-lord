f3f986e7711c468244284ce20979181d428a7047
Savor d'Isavano <anohigisavay@gmail.com>
2016-11-17 19:39:56 +0800

    A tiny timer component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,39 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <timer ref="tmr" :decimals="1"></timer>
+    <button type="button" @click="startTimer">Start timer</button>
+    <button type="button" @click="stopTimer">Stop timer</button>
+    <button type="button" @click="clearTimer">Clear timer</button>
+    <button type="button" @click="restartTimer">Restart timer</button>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Timer from './components/Timer'
 
 export default {
   name: 'app',
+  created () {
+  },
+  methods: {
+    startTimer () {
+      this.$refs.tmr.start()
+    },
+    stopTimer () {
+      this.$refs.tmr.stop()
+    },
+    clearTimer () {
+      this.$refs.tmr.clear()
+    },
+    restartTimer () {
+      this.$refs.tmr.restart()
+    }
+  },
   components: {
-    Hello
+    Hello,
+    Timer
   }
 }
 </script>
---
+++ src/components/Timer.vue
@@ -0,0 +1,45 @@
+<template>
+  <span>{{ elapsed.toFixed(decimals) }}</span>
+</template>
+
+<script>
+  export default {
+    name: 'timer',
+    props: {
+      unit: {
+        type: Number,
+        default: 0.01
+      },
+      decimals: {
+        type: Number,
+        default: 0
+      }
+    },
+    data () {
+      return {
+        elapsed: 0,
+        decimals: 0
+      }
+    },
+    methods: {
+      start () {
+        this.tmr = setInterval(() => {
+          this.elapsed += this.unit
+        }, this.unit * 1000)
+      },
+      stop () {
+        if (this.tmr !== undefined) {
+          clearInterval(this.tmr)
+        }
+      },
+      clear () {
+        this.stop()
+        this.elapsed = 0
+      },
+      restart () {
+        this.clear()
+        this.start()
+      }
+    }
+  }
+</script>
